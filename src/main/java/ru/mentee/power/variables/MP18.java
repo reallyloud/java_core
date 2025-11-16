29c9a7e59b641b8510e26ed4ed0c54e9e444fdb2
Bark Maxim <maximalen1999@gmail.com>
2025-11-16 19:39:48 +0300

    MP18: Починил Git репозиторий

--- src/main/java/ru/mentee/power/variables/MP18.java
+++ src/main/java/ru/mentee/power/variables/MP18.java
@@ -1,5 +1,5 @@
 package ru.mentee.power.variables;
-public class MP18 {
+public class MP18  {
     public static void main(String[] args) {
         int myAge = 23;
         String myName = "Максим";
