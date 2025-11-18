8884ef752cccb26c4fd05fb73e405769ab8fdef7
Bark Maxim <maximalen1999@gmail.com>
2025-11-18 11:54:57 +0300

    MP-20: Типы данных, задание выполнено

---
+++ src/main/java/ru/mentee/power/datatypes/MP20.java
@@ -0,0 +1,18 @@
+package ru.mentee.power.datatypes;
+public class MP20 {
+    public static void main(String[] args) {
+        int i = 3;
+        byte b = 2;
+        long l = 53;
+        double dbl = 3.5;
+        String str1 = "Привет";
+        String str2 = "Пока";
+        int[] intArray = {5, 7, 3, 4, 4};
+        String[] strArray = {"Максим", "Геннадий", "Алексей"};
+
+        System.out.println("int(3) + byte(2) = " + (i + b));
+        System.out.println("int(3) / byte(2) = " + (i / b));
+        System.out.println("double(3.5) / byte(2) = " + (dbl / b));
+        System.out.println("(Преобразование) int(3) * dbl(3.5) = " + (i * dbl));
+    }
+}
